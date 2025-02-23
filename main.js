996bc9a06a9d45dfe5bee9f6f5ef6ff84c72c56b
dgallagher <duncang42@gmail.com>
2025-02-23 17:07:54 -0600

    added drawing and grid selection, need to fix reset

---
+++ main.js
@@ -0,0 +1,52 @@
+function enableDrawing() {
+    const cells = document.querySelectorAll(".sketch-square");
+    const state = {
+        mousePressed: false,
+    };
+
+    document.addEventListener("mousedown", () => {
+        state.mousePressed = true;
+    });
+
+    document.addEventListener("mouseup", () => {
+        state.mousePressed = false;
+    });
+
+    cells.forEach((cell) => {
+        cell.addEventListener("mouseenter", () => {
+            if (state.mousePressed) {
+                cell.classList.add("filled")
+            }
+        });
+
+        cell.addEventListener("click", () => {
+            cell.classList.toggle("filled")
+        })
+    });
+}
+
+function resetGrid() {
+    document.getElementById('sketchContainer').innerHTML = '';
+    setTimeout(() => {
+        createGrid();
+    }, "1000");
+}
+
+function createGrid() {
+    console.log('createGrid run');
+    // resetGrid();
+    let input = document.getElementById('gridSelection').value;
+    let number_of_squares = input * input;
+    const sketch_container = document.getElementById('sketchContainer');
+    let square_width = sketch_container.offsetWidth / input;
+    let square_height = sketch_container.offsetHeight / input;
+
+    for (let i = 0; i < number_of_squares; i++) {
+        let child_node = document.createElement('div');
+        child_node.classList.add('sketch-square');
+        child_node.style.height = square_height.toString() + 'px';
+        child_node.style.width = square_width.toString() + 'px';
+        sketch_container.appendChild(child_node);
+    }
+    enableDrawing();
+}
